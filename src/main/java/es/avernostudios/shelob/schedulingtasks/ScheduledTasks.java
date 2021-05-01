fbe24143291863480f0501ed13035fed06adafe3
yrch <davidortegaingeniero@gmail.com>
2021-05-01 12:31:23 +0200

    Añado docker-compose.yml

--- src/main/java/es/avernostudios/shelob/schedulingtasks/ScheduledTasks.java
+++ src/main/java/es/avernostudios/shelob/schedulingtasks/ScheduledTasks.java
@@ -1,10 +1,13 @@
 package es.avernostudios.shelob.schedulingtasks;
 
+import es.avernostudios.shelob.services.FichajeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.net.MalformedURLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -20,8 +23,16 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
+
+    @Autowired
+    FichajeService fichajeService;
+
     @Scheduled(cron = "${crontabSchedule}")
-    public void reportCurrentTime() {
+    public void reportCurrentTime() throws MalformedURLException {
+        log.info("crontabSchedule: '{}' ", crontabSchedule);
         log.info("The time is now {}", dateFormat.format(new Date()));
+
+        fichajeService.work();
+
     }
 }
